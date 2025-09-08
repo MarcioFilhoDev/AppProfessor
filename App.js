06dba3e210bcdf7d6596f4ec452eedb3c0baf72f
MarcioFilhoDev <marciofilho.dev@hotmail.com>
2025-09-08 17:33:54 -0300

    Configurado as rotas

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { Text, View } from 'react-native';
-import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './src/routes/routes';
 
 export default function App() {
   return (
-    <View>
-      <Text>Tela home</Text>
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
