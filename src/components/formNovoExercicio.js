74a3c87a55c9d44417c8103a058e0140f1e06117
MarcioFilhoDev <marciofilho.dev@hotmail.com>
2025-09-12 16:33:34 -0300

    - Ajustado "componentes" da tela de Treinos

--- src/components/formNovoExercicio.js
+++ src/components/formNovoExercicio.js
@@ -1,4 +1,13 @@
-import { Alert, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, { useContext, useState } from 'react';
 
 import { ExerciciosContext } from '../contexts/exercicios';
@@ -55,93 +64,106 @@ export default function FormNovoExercicio() {
     setNivel('');
   }
 
+  // <ScrollView className="flex-1"></ScrollView>
   return (
-    <View className="flex-1 items-center w-full ">
-      <View className="w-4/5 h-full justify-end gap-6">
-        <Text className="text-center text-lg">Criando um novo exercício</Text>
-        {/* Definindo o nome do exercicio */}
-        <View className="flex-row items-center w-full gap-2 ">
-          <Text>Nome do exercicio: </Text>
-          <TextInput
-            placeholder="Ex: supino reto"
-            placeholderTextColor={'#7a7b7d'}
-            className="bg-white flex-1 elevation rounded p-2"
-            maxLength={50}
-            value={nomeExercicio}
-            onChangeText={text => setNomeExercicio(text)}
-          />
-        </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1, alignItems: 'center' }}
+        showsVerticalScrollIndicator={false}
+      >
+        {/* <View className="flex-1 items-center w-full "> */}
+        <View className="w-4/5 h-full justify-end gap-6">
+          <Text className="text-center text-lg">Criando um novo exercício</Text>
+          {/* Definindo o nome do exercicio */}
+          <View className="flex-row items-center w-full gap-2 ">
+            <Text>Nome do exercicio: </Text>
+            <TextInput
+              placeholder="Ex: supino reto"
+              placeholderTextColor={'#7a7b7d'}
+              className="bg-white flex-1 elevation rounded p-2"
+              maxLength={50}
+              value={nomeExercicio}
+              onChangeText={text => setNomeExercicio(text)}
+            />
+          </View>
 
-        {/* Definindo grupo muscular */}
-        <View className="flex-col  w-full gap-2">
-          <Text>Selecione o grupo muscular: </Text>
+          {/* Definindo grupo muscular */}
+          <View className="flex-col  w-full gap-2">
+            <Text>Selecione o grupo muscular: </Text>
 
-          <View className="flex-row gap-4">
+            <View className="flex-row gap-4">
+              <TouchableOpacity
+                onPress={() => setGrupoMuscular('Inferiores')}
+                className={`bg-white flex-1 items-center elevation rounded p-2 border-2 ${
+                  grupoMuscular === 'Inferiores'
+                    ? 'border-primary'
+                    : 'border-white'
+                }`}
+              >
+                <Text>Inferiroes</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={() => setGrupoMuscular('Superiores')}
+                className={`bg-white flex-1 items-center elevation rounded p-2 border-2 ${
+                  grupoMuscular === 'Superiores'
+                    ? 'border-primary'
+                    : 'border-white'
+                }`}
+              >
+                <Text>Superiores</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={() => setGrupoMuscular('Abdomen')}
+                className={`bg-white flex-1 items-center elevation rounded p-2 border-2 ${
+                  grupoMuscular === 'Abdomen'
+                    ? 'border-primary'
+                    : 'border-white'
+                }`}
+              >
+                <Text>Abdomen</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {/* Definindo nivel do exercicio */}
+          <View className="flex-row justify-between">
+            {niveis.map(item => {
+              return (
+                <View key={item.nivel} className="items-center gap-1.5">
+                  <TouchableOpacity
+                    onPress={() => setNivel(item.classificacao)}
+                    className={`bg-white items-center elevation rounded-lg px-6 py-4 border-2 ${
+                      nivel === item.classificacao
+                        ? 'border-primary'
+                        : 'border-white'
+                    }`}
+                  >
+                    <Text className="font-medium">{item.nivel}</Text>
+                  </TouchableOpacity>
+
+                  <Text>{item.classificacao}</Text>
+                </View>
+              );
+            })}
+          </View>
+
+          {/* Botão para salvar exercicio */}
+          <View className="items-center">
             <TouchableOpacity
-              onPress={() => setGrupoMuscular('Inferiores')}
-              className={`bg-white flex-1 items-center elevation rounded p-2 border-2 ${
-                grupoMuscular === 'Inferiores'
-                  ? 'border-primary'
-                  : 'border-white'
-              }`}
+              onPress={salvandoExercicio}
+              className="bg-primary items-center w-1/2 p-3.5 rounded-lg elevation"
             >
-              <Text>Inferiroes</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              onPress={() => setGrupoMuscular('Superiores')}
-              className={`bg-white flex-1 items-center elevation rounded p-2 border-2 ${
-                grupoMuscular === 'Superiores'
-                  ? 'border-primary'
-                  : 'border-white'
-              }`}
-            >
-              <Text>Superiores</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              onPress={() => setGrupoMuscular('Abdomen')}
-              className={`bg-white flex-1 items-center elevation rounded p-2 border-2 ${
-                grupoMuscular === 'Abdomen' ? 'border-primary' : 'border-white'
-              }`}
-            >
-              <Text>Abdomen</Text>
+              <Text className="font-medium text-white">Salvar</Text>
             </TouchableOpacity>
           </View>
         </View>
-
-        {/* Definindo nivel do exercicio */}
-        <View className="flex-row justify-between">
-          {niveis.map(item => {
-            return (
-              <View key={item.nivel} className="items-center gap-1.5">
-                <TouchableOpacity
-                  onPress={() => setNivel(item.classificacao)}
-                  className={`bg-white items-center elevation rounded-lg px-6 py-4 border-2 ${
-                    nivel === item.classificacao
-                      ? 'border-primary'
-                      : 'border-white'
-                  }`}
-                >
-                  <Text className="font-medium">{item.nivel}</Text>
-                </TouchableOpacity>
-
-                <Text>{item.classificacao}</Text>
-              </View>
-            );
-          })}
-        </View>
-
-        {/* Botão para salvar exercicio */}
-        <View className="items-center">
-          <TouchableOpacity
-            onPress={salvandoExercicio}
-            className="bg-primary items-center w-1/2 p-3.5 rounded-lg elevation"
-          >
-            <Text className="font-medium text-white">Salvar</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
+        {/* </View> */}
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/appScreens/treinos.js
+++ src/screens/appScreens/treinos.js
@@ -8,7 +8,6 @@ import {
   Modal,
   KeyboardAvoidingView,
   Platform,
-  StatusBar,
 } from 'react-native';
 import { useContext, useState } from 'react';
 
@@ -29,88 +28,78 @@ export default function Treinos() {
   }
 
   return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-    >
-      <TouchableWithoutFeedback
-        onPress={() => Keyboard.dismiss()}
-        touchSoundDisabled
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <KeyboardAvoidingView
+        className="flex-1"
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
       >
-        <ScrollView
-          contentContainerStyle={{ flexGrow: 1 }}
-          keyboardShouldPersistTaps="handled"
-        >
-          <View className="flex-1 gap-4">
-            {/* Formulario para cadastrar novo exercicio */}
-            <FormNovoExercicio />
+        <View className="flex-1 gap-4">
+          {/* Formulário */}
+          <FormNovoExercicio />
 
-            <View className="items-center w-full px-4">
-              <View className="bg-black/20 w-full h-0.5 rounded-xl" />
-            </View>
-
-            {/* Lista de exercicios */}
-            <ScrollView className="flex-1 bg-gray-100">
-              <View className="w-full items-center">
-                <View className="w-11/12 rounded-md">
-                  <View className="flex-row bg-primary rounded-t-md">
-                    <Text className="flex-1 text-white text-base font-bold text-center py-2 border-r border-orange-300">
-                      Exercícios
-                    </Text>
-                    <Text className="flex-1 text-white text-base font-bold text-center py-2 border-r border-orange-300">
-                      Grupo Muscular
-                    </Text>
-                    <Text className="flex-1 text-white text-base font-bold text-center py-2 border-orange-300">
-                      Nível
-                    </Text>
-                  </View>
-
-                  {exercicios.map(item => (
-                    // Quando pressionar um botão abre um modal com as informacoes
-                    // Sendo possivel deletar ou editar os dados
-                    <TouchableOpacity
-                      onPress={() => abrindoModal(item)}
-                      activeOpacity={0.5}
-                      key={item.id}
-                      className="flex-row items-stretch bg-white elevation border-b border-orange-400 rounded-b-md"
-                    >
-                      <Text className="flex-1 text-secondary text-center py-2 border-r border-orange-400 self-stretch">
-                        {item.nome}
-                      </Text>
-
-                      <Text className="flex-1 text-secondary text-center py-2 border-r border-orange-400 self-stretch">
-                        {item.grupoMuscular}
-                      </Text>
-
-                      <Text className="flex-1 text-secondary text-center py-2 self-stretch">
-                        {item.nivel}
-                      </Text>
-                    </TouchableOpacity>
-                  ))}
-                </View>
-              </View>
-            </ScrollView>
-
-            <Modal
-              visible={statusModal}
-              animationType="slide"
-              transparent={true}
-            >
-              {exercicioSelecionado && (
-                <DetalhesExercicio
-                  fecharModal={() => setStatusModal(false)}
-                  nome={exercicioSelecionado.nome}
-                  grupoMuscular={exercicioSelecionado.grupoMuscular}
-                  nivel={exercicioSelecionado.nivel}
-                  usuario={exercicioSelecionado.cadastrado_por}
-                  id={exercicioSelecionado.id}
-                  deletarExercicio={deletarExercicio}
-                />
-              )}
-            </Modal>
+          <View className="items-center w-full px-4">
+            <View className="bg-black/20 w-full h-0.5 rounded-xl" />
           </View>
-        </ScrollView>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+
+          {/* Tabela */}
+          <View className="flex-1 px-4">
+            <View
+              style={{ height: '90%' }}
+              className="w-full bg-gray-100 rounded-md overflow-hidden"
+            >
+              {/* Cabeçalho da tabela */}
+              <View className="flex-row bg-primary rounded-t-md">
+                <Text className="flex-1 text-white text-base font-bold text-center py-2 border-r border-orange-300">
+                  Exercícios
+                </Text>
+                <Text className="flex-1 text-white text-base font-bold text-center py-2 border-r border-orange-300">
+                  Grupo Muscular
+                </Text>
+                <Text className="flex-1 text-white text-base font-bold text-center py-2 border-orange-300">
+                  Nível
+                </Text>
+              </View>
+
+              {/* Lista de exercicios criados */}
+              <ScrollView>
+                {exercicios.map(item => (
+                  <TouchableOpacity
+                    onPress={() => abrindoModal(item)}
+                    activeOpacity={0.5}
+                    key={item.id}
+                    className="flex-row items-stretch bg-white elevation border-b border-orange-400"
+                  >
+                    <Text className="flex-1 text-secondary text-center py-2 border-r border-orange-400 self-stretch">
+                      {item.nome}
+                    </Text>
+                    <Text className="flex-1 text-secondary text-center py-2 border-r border-orange-400 self-stretch">
+                      {item.grupoMuscular}
+                    </Text>
+                    <Text className="flex-1 text-secondary text-center py-2 self-stretch">
+                      {item.nivel}
+                    </Text>
+                  </TouchableOpacity>
+                ))}
+              </ScrollView>
+            </View>
+          </View>
+
+          {/* Modal com detalhes */}
+          <Modal visible={statusModal} animationType="slide" transparent={true}>
+            {exercicioSelecionado && (
+              <DetalhesExercicio
+                fecharModal={() => setStatusModal(false)}
+                nome={exercicioSelecionado.nome}
+                grupoMuscular={exercicioSelecionado.grupoMuscular}
+                nivel={exercicioSelecionado.nivel}
+                usuario={exercicioSelecionado.cadastrado_por}
+                id={exercicioSelecionado.id}
+                deletarExercicio={deletarExercicio}
+              />
+            )}
+          </Modal>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
